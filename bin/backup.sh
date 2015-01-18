55661dbb7127d3f6cffeb6e253d879bc73a2fa97
Austin <austin.guiney@gmail.com>
2015-01-17 19:59:37 -0500

    backup.sh now commits changes to the master branch before switching back to the dev branch.

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/austin/Desktop/shootout/ && git add -u && git reset HEAD -- /home/austin/Desktop/shootout/src && git commit -m "Automatic Cron Backup."
+cd /home/austin/Desktop/shootout/ && git checkout master && git add -u && git reset HEAD -- /home/austin/Desktop/shootout/src && git commit -m "Automatic Cron Backup." && git checkout dev
