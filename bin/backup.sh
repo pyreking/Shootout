86d8a922d2b25c9c3462c5efd48a5ae6cd562fed
Austin <austin.guiney@gmail.com>
2015-01-17 20:54:14 -0500

    Added newlines and comments.

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-cd /home/austin/Desktop/shootout/ && git checkout master && git add -u && git reset HEAD -- /home/austin/Desktop/shootout/src && git commit -m "Automatic Cron Backup." && git checkout dev
+cd /home/austin/Desktop/shootout/
+# Use the master branch for commiting changes.
+git checkout master
+# Add modified files to the staging area.
+git add -u
+# Remove src folder from the staging area.
+git reset HEAD -- /home/austin/Desktop/shootout/src
+# Commit the changes.
+git commit -m "Automatic Cron Backup."
+# Switch to the dev branch.
+git checkout dev
