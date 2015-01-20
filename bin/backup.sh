3960e30b922b1f5df4ac6deacebeb4b7dce8a49d
Austin <austin.guiney@gmail.com>
2015-01-19 23:56:30 -0500

    Removed the git branch commands. Some updates were being commited to the wrong branch.

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 cd /home/austin/Desktop/shootout/
-# Use the master branch for commiting changes.
-git checkout master
 # Add modified files to the staging area.
 git add -u
 # Remove src folder from the staging area.
 git reset HEAD -- /home/austin/Desktop/shootout/src
 # Commit the changes.
 git commit -m "Automatic Cron Backup."
-# Switch to the dev branch.
-git checkout dev
