07f11c518befc66cbd30ec03095119a1637de87c
Austin <austin.guiney@gmail.com>
2015-03-15 17:40:07 -0400

    Updated bin/backup.sh to only commit changes for a small list of files.

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+# Go to the project directory
 DIR="/home/austin/Desktop/shootout"
 
 cd $DIR/
 
-# Add modified files to the staging area.
-
-git add -u
-
-# Remove the selected directories from the staging area.
-
-git reset HEAD -- $DIR/shootout/src
-git reset HEAD -- $DIR/bin
-git reset HEAD -- $DIR/gnuplot/*.gp
+# Add the specified files to the staging area.
+git add $DIR/gnuplot/graphs/Game.svg
+git add $DIR/gnuplot/graphs/Length.svg
+git add $DIR/gnuplot/graphs/Season.svg
+git add $DIR/summary/change_log.csv
+git add $DIR/summary/current.csv
+git add $DIR/summary/current.html
+git add $DIR/shots/shots.csv
+git add $DIR/shots/shots.html
 
 # Commit the changes.
 git commit -m "Automatic Cron Backup."
