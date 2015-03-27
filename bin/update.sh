92a30984ec7b5d8ddab1e3779977f1ed0debacd6
Austin <austin.guiney@gmail.com>
2015-03-26 20:16:31 -0400

    Updated the reference names and arguments for the JAR files.

--- bin/update.sh
+++ bin/update.sh
@@ -14,11 +14,11 @@ fi
 sh /home/austin/Desktop/shootout/bin/pull_files.sh
 
 # Parse the HTML tables into csv files.
-java -jar /home/austin/Desktop/shootout/bin/HTMLParser.jar
-java -jar /home/austin/Desktop/shootout/bin/HTMLParser.jar -directory /home/austin/Desktop/shootout/shots/ -input shots.html -output shots.csv
+java -jar /home/austin/Desktop/shootout/bin/TableParser.jar -path /home/austin/Desktop/shootout/summary/ -file current.html -output current.csv
+java -jar /home/austin/Desktop/shootout/bin/TableParser.jar -path /home/austin/Desktop/shootout/shots/ -file shots.html -output shots.csv
 
-# Update the change log.
-java -jar /home/austin/Desktop/shootout/bin/ChangeLog.jar
+# Update the changelog.
+java -jar /home/austin/Desktop/shootout/bin/Changelog.jar -path /home/austin/Desktop/shootout/summary/ -file current.html -ouput changelog.csv
 
 # Update the graphs.
 gnuplot /home/austin/Desktop/shootout/gnuplot/graph_game.gp | echo "Updated graph_game.gp"
