40b4dc71790ed4ba2c83f4b73decb22d493966bb
Austin <austin.guiney@gmail.com>
2015-03-26 20:02:49 -0400

    Made a few changes. - Updated argument and exception descriptions. - The file argument no longer calls Option.builder.required(). - The file option is still required, but only when the help argument has not been used. Otherwise, it throws a MissingArgumentException.

--- src/com/tumblr/aguiney/html/nhl/TableParser.java
+++ src/com/tumblr/aguiney/html/nhl/TableParser.java
@@ -1,4 +1,4 @@
-package com.tumblr.aguiney.htmlparser;
+package com.tumblr.aguiney.html.nhl;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,79 +11,90 @@ import org.jsoup.nodes.*;
 import org.jsoup.select.*;
 import org.apache.commons.cli.*;
 
-public class HTMLParser {
+public class TableParser {
 
 	public static void main(String[] args) {
-			String path = System.getProperty("user.dir") + "/";
-			String location = path;
-			String inputFile = "table.html";
-			String outputFile = "table.csv";
-			String delimiter = ",";
+		String path = System.getProperty("user.dir") + "/";
+		String location = path;
+		String inputName = "table.html";
+		String outputName = "table.csv";
+		String delimiter = ",";
+		
+		Options options = new Options();
+		CommandLineParser parser = new DefaultParser();
+		
+		options.addOption("p", "path", true, 
+				"The absolute or relative path containing the HTML file "
+				+ "to parse. The default path is the current directory.");
+		options.addOption("l", "location", true,
+				"The absolute or relative path for the CSV file. "
+				+ "The default location is the current directory "
+				+ "or specified path.");
+		options.addOption("f", "file", true, "An HTML file to parse. Required, "
+				+ "except when using the help option.");
+		options.addOption("o", "output", true, 
+				"A name for the CSV file.");
+		options.addOption("d", "delimiter", true, 
+				"A data seperator for the table.");
+		options.addOption("h", "help", false, "Print this message and quit. "
+				+ "Cannot be used with another argument.");
 			
-			Options options = new Options();
-			CommandLineParser parser = new DefaultParser();
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			HelpFormatter help = new HelpFormatter();
+			String newline = System.lineSeparator();
+			String header = newline + "A Java program that parses an "
+					+ "HTML table into a CSV file. A \"Date Modified\" " +
+					"stamp is added at the end of the CSV file. " +
+					"Uses the JSoup and Commons CLI libraries." + newline 
+					+ newline;
+			String footer = newline + "Please report any issues at "
+					+ "https://github.com/pyreking/Shootout/issues" + ".";
+				
+			int length = cmd.getOptions().length;
 			
-			options.addOption("p", "path", true, 
-					"absolute or relative path containing "
-					+ "the pre-parsed HTML files");
-			options.addOption("l", "location", true,
-					"absolute or relative path for the resulting file");
-			options.addOption(Option.builder("f").longOpt("file").
-					hasArg().required().desc("the pre-parsed HTML file").build());
-			options.addOption("o", "output", true, 
-					"name for the parsed HTML file");
-			options.addOption("d", "delimiter", true, 
-					"data seperator for the table");
-			options.addOption("h", "help", false, "print a list of commands and quit");
+			if (cmd.hasOption("h")) {
+				if (length == 1) {
+					help.printHelp("java -jar TableParser.jar", 
+							header, options, footer, true);
+					System.exit(0);
+				} else {
+					throw new ParseException("-h is a "
+							+ "mutually exclusive argument.");
+				}
+			}
 			
-			try {
-				CommandLine cmd = parser.parse(options, args);
-				HelpFormatter help = new HelpFormatter();
-				String newline = System.lineSeparator();
-				String header = newline + "A Java program that parses an "
-						+ "HTML table into a CSV file. A \"Date Modified\" " +
-						"stamp is added at the end of the CSV file. " +
-						"Uses the JSoup and Commons CLI libraries." + newline 
-						+ newline;
-				String footer = newline + "Please report any issues at "
-						+ "https://github.com/pyreking/Shootout/issues" + ".";
+			if (!cmd.hasOption("f")) {
+				throw new MissingOptionException("-f is required, except"
+						+ "when using -h.");
+			} else {
+				inputName = cmd.getOptionValue("f");
+			}
+			
+			if (cmd.hasOption("p")) {
+				path = cmd.getOptionValue("p");
+				location = path;
+			}
 				
-				int length = cmd.getOptions().length;
-				
-				if (cmd.hasOption("h")) {
-					if (length == 1) {
-						help.printHelp("java -jar HTMLParser.jar", 
-								header, options, footer, true);
-						System.exit(0);
-					} else {
-						throw new ParseException("-help is a stand-alone argument.");
-					}
-				}
-				
-				if (cmd.hasOption("p")) {
-					path = cmd.getOptionValue("p");
-					location = path;
-				}
-				if (cmd.hasOption("l")) {
-					location = cmd.getOptionValue("l");
-				}
-				if (cmd.hasOption("f")) {
-					inputFile = cmd.getOptionValue("f");
-				}
-				if (cmd.hasOption("o")) {
-					outputFile = cmd.getOptionValue("o");
-				}
-				if (cmd.hasOption("d")) {
-					delimiter = cmd.getOptionValue("d");
-				}
+			if (cmd.hasOption("l")) {
+				location = cmd.getOptionValue("l");
+			}
+			
+			if (cmd.hasOption("o")) {
+				outputName = cmd.getOptionValue("o");
+			}
+			
+			if (cmd.hasOption("d")) {
+				delimiter = cmd.getOptionValue("d");
+			}
 				
 			} catch (ParseException pe) {
 				System.err.println("Error: " + pe.getMessage());
 				System.exit(1);
 			}
-			
-			File input = new File(path + inputFile);
-			File output = new File(location + outputFile);
+		
+			File input = new File(path + inputName);
+			File output = new File(location + outputName);
 			
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("MMM d y hh:mm:ss a z");
